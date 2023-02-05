bc0607b8612b7594a6cbc9cc4c7939367f4230e9
Bakhodir <bilalovbakhodir@gmail.com>
2023-02-05 14:55:48 +0600

    My first py programm

---
+++ main.py
@@ -0,0 +1,9 @@
+# Для вывода данных используем команду print
+print("Bilalov Bakhodir Boburovich")
+print("You are student?")
+print("Press 1 if yes, otherwise 2")
+n = int(input())
+if (n == 1):
+    print("It's perfect")
+else:
+    print("don't worry")
